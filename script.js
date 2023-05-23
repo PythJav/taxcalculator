dfc926295f2d53a3be3a7ed3ca543434362dcc5f
CC_Work <127169630+PythJav@users.noreply.github.com>
2023-05-23 17:47:07 +0700

    Add ptkp value

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const salary = document.getElementById("salary");
 const tax = document.getElementById("yourTax");
 const healthDe = document.getElementById("healthDe");
 const healthInc = document.getElementById("healthInc");
+const ptkp = document.getElementById("ptkp");
+const ptkpVal = document.getElementById("ptkpList");
+
+
 
 
 function calcTax(){
@@ -11,6 +15,7 @@ function calcTax(){
     let healthDeCalc= Math.min(95596,(salary.value*0.01));
     let healthIncCalc= Math.min(480000,(salary.value*0.04));
     healthDe.innerHTML=healthDeCalc.toLocaleString('en-US');
+    ptkp.innerHTML=ptkpVal.value.toLocaleString('en-US');
     calc21(realSal);
     // tax.innerHTML=taxCalc.toLocaleString('en-US');
     healthInc.innerHTML=healthIncCalc.toLocaleString('en-US');

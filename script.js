a944688bac5379f498c431127e6606b014c5a21f
CC_Work <127169630+PythJav@users.noreply.github.com>
2023-05-23 16:26:49 +0700

    Hide calculations by until button click

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const salary = document.getElementById("salary");
 const tax = document.getElementById("yourTax");
-const health = document.getElementById("health");
+const healthDe = document.getElementById("healthDe");
+const healthInc = document.getElementById("healthInc");
 
 
 function calcTax(){
+    document.getElementById("calc").style.display = "block";
     let taxCalc= (parseFloat(salary.value*0.15));
-    let healthCalc= Math.min(95596,(salary.value*0.01));
-    health.innerHTML=healthCalc;
-    tax.innerHTML=taxCalc;
+    let healthDeCalc= Math.min(95596,(salary.value*0.01));
+    let healthIncCalc= Math.min(480000,(salary.value*0.04));
+    healthDe.innerHTML=healthDeCalc.toLocaleString('en-US');
+    tax.innerHTML=taxCalc.toLocaleString('en-US');
+    healthInc.innerHTML=healthIncCalc.toLocaleString('en-US');
 
 
 }

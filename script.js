6d97bf7faa756811d8f15d1ac161ab1fd802865e
CC_Work <127169630+PythJav@users.noreply.github.com>
2023-05-24 12:05:31 +0700

    Add months worked

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ const healthDe = document.getElementById("healthDe");
 const healthInc = document.getElementById("healthInc");
 const ptkp = document.getElementById("ptkp");
 const ptkpVal = document.getElementById("ptkpList");
+const sMonth = document.getElementById("sMonth");
+const eMonth = document.getElementById("eMonth");
+const calcmWork = document.getElementById("calcmWork");
 
 
 
 
 function calcTax(){
+  
     document.getElementById("calc").style.display = "block";
     const realSal = salary.value;
+    findPtkp();
+    
+    calcMonth();
     let taxCalc= (parseFloat(salary.value*0.15));
     let healthDeCalc= Math.min(95596,(salary.value*0.01));
     let healthIncCalc= Math.min(480000,(salary.value*0.04));
     healthDe.innerHTML=healthDeCalc.toLocaleString('en-US');
-    ptkp.innerHTML=parseInt(ptkpVal.value).toLocaleString('en-US');
     calc21(realSal);
+    
     // tax.innerHTML=taxCalc.toLocaleString('en-US');
     healthInc.innerHTML=healthIncCalc.toLocaleString('en-US');
 
@@ -29,7 +36,19 @@ const brackets = [
     { rate: 0.30, maxAmount: 5000000000    },
     { rate: 0.35, maxAmount: Infinity    },
   ];
+function findPtkp(){
+  ptkp.innerHTML=parseInt(ptkpVal.value).toLocaleString('en-US');
 
+
+}
+function calcMonth(){
+ 
+  let mWorked = eMonth.value-sMonth.value+1;
+  calcmWork.innerHTML=mWorked;
+  
+  
+
+}
 
 function calc21(income){
     let incomeTax = 0;
